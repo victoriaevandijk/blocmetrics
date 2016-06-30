1e0b2a0015781488c985797dd6e979838bbf1f8c
Torie <victoriaevandijk@gmail.com>
2016-06-30 02:25:20 +0000

    Deleted Migrations

--- db/migrate/20160526011506_add_registered_application_to_event.rb
+++
@@ -1,6 +0,0 @@
-class AddRegisteredApplicationToEvent < ActiveRecord::Migration
-  def change
-    add_column :events, :registered_application_id, :integer
-    add_index :events, :registered_application_id 
-  end
-end
